8580062101ae4f140b3cd73c38fc2ddccf16a247
mt-coff <mt.coff.00@gmail.com>
2020-08-11 14:58:06 +0900

    history.go検証動作確認用

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 (() => {
   console.log(window.history);
+  const go = document.querySelector("#go");
+  go.addEventListener(
+    "click",
+    () => {
+      window.history.go();
+    },
+    false
+  );
+  window.setTimeout(() => {
+    window.history.go();
+  }, 10000);
 })();
